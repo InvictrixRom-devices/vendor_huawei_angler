9666c61a17fa4b1b0a672b1b1e152e3ad0894740
Josh Chasky <jchasky@gmail.com>
2016-11-08 14:09:02 -0500

    Angler: Update blobs to Android 7.0 (NBD91K)
    Change-Id: I104adb9961e8e51b516b0a2a81021920bd7d838a

--- angler/device-vendor.mk
+++ angler/device-vendor.mk
@@ -1,35 +1,33 @@
-# [2016-10-08] Auto-generated file, do not edit
+# Copyright (C) 2015-2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 $(call inherit-product, vendor/huawei/angler/angler-vendor-blobs.mk)
 
-# Prebuilt APKs/JARs from 'vendor/app'
-PRODUCT_PACKAGES += \
-    ims \
-    TimeService
-
-# Prebuilt APKs libs symlinks from 'vendor/app'
-PRODUCT_PACKAGES += \
-    libimscamera_jni_64.so \
-    libimsmedia_jni_64.so
-
-# Prebuilt APKs/JARs from 'proprietary/app'
+# Prebuilt APKs
 PRODUCT_PACKAGES += \
     HwMMITest \
     HwSarControlService \
+    ims \
     SetupSmartDeviceOverlay \
+    TimeService \
     Tycho
 
-# Prebuilt APKs libs symlinks from 'proprietary/app'
+# Prebuilt jars
 PRODUCT_PACKAGES += \
-    libManufacture_64.so
-
-# Prebuilt APKs/JARs from 'proprietary/framework'
-PRODUCT_PACKAGES += \
-    com.google.android.camera.experimental2015 \
-    com.google.widevine.software.drm \
     qcrilhook
 
-# Prebuilt APKs/JARs from 'proprietary/priv-app'
+# Prebuilt privileged APKs
 PRODUCT_PACKAGES += \
     CallStatistics \
     CNEService \
@@ -43,26 +41,10 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     SprintDM
 
-# Prebuilt APKs libs symlinks from 'proprietary/priv-app'
+# Symlinks
 PRODUCT_PACKAGES += \
-    libdmengine_32.so \
-    libdmjavaplugin_32.so
-
-# Standalone symbolic links
-PRODUCT_PACKAGES += \
-    lowi.conf
-
-# Prebuilt shared libraries
-PRODUCT_PACKAGES += \
-    libdsi_netctrl \
-    libloc_api_v02 \
-    libloc_ds_api \
-    libqmi_cci \
-    libqmi_common_so \
-    libqmi_csi \
-    libqmiservices
-
-# Extra modules from user configuration
-PRODUCT_PACKAGES += \
-    ETC_AOSPLinks
-
+    libimsmedia_jni.so \
+    libimscamera_jni.so \
+    libdmengine.so \
+    libdmjavaplugin.so \
+    libManufacture.so
